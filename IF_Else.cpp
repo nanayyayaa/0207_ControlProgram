2cf4fc20c86ce22864b42bd50db63f70bde8d86a
nanayyayaa <jiejueejuju@gmail.com>
2026-03-09 20:06:34 +0700

    IFelse2

--- IF_Else.cpp
+++ IF_Else.cpp
@@ -4,3 +4,10 @@ using namespace std;
 float Rata_rata(float a, floatb){
     return (a+b)/2;
 }
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
